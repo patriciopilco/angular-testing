af3b4af1c856b4fb3323692115c6d71a8ac820f5
Nicolas Molina <hi@nicobytes.com>
2025-02-21 15:33:53 -0500

    Refactor product component with input alias and add unit test

---
+++ src/app/domains/products/components/product/product.component.spec.ts
@@ -0,0 +1,24 @@
+import { createRoutingFactory, Spectator } from '@ngneat/spectator/jest';
+
+import { ProductComponent } from './product.component';
+import { generateFakeProduct } from '@shared/models/product.mock';
+
+describe('ProductComponent', () => {
+  let spectator: Spectator<ProductComponent>;
+
+  const createComponent = createRoutingFactory({
+    component: ProductComponent,
+  });
+
+  beforeEach(() => {
+    spectator = createComponent({
+      detectChanges: false,
+    });
+    spectator.setInput('product', generateFakeProduct());
+  });
+
+  it('should create', () => {
+    spectator.detectChanges();
+    expect(spectator.component).toBeTruthy();
+  });
+});
