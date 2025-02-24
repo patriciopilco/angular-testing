73b60738e25d5f7f06f2ee9dab27765f4ae3aabd
Nicolas Molina <hi@nicobytes.com>
2025-02-24 10:47:44 -0500

    Enhance ProductDetailComponent spec with related products and IntersectionObserver mocking

--- src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.spec.ts
@@ -9,6 +9,18 @@ import ProductDetailComponent from './product-detail.component';
 import { ProductService } from '@shared/services/product.service';
 import { generateFakeProduct } from '@shared/models/product.mock';
 import { of } from 'rxjs';
+import { DeferBlockBehavior } from '@angular/core/testing';
+import { RelatedComponent } from '../../components/related/related.component';
+
+window.IntersectionObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn(),
+  takeRecords: jest.fn(),
+  root: null,
+  rootMargin: '',
+  thresholds: [],
+}));
 
 describe('ProductDetailComponent', () => {
   let spectator: Spectator<ProductDetailComponent>;
@@ -17,6 +29,7 @@ describe('ProductDetailComponent', () => {
 
   const createComponent = createRoutingFactory({
     component: ProductDetailComponent,
+    deferBlockBehavior: DeferBlockBehavior.Manual,
     providers: [
       mockProvider(ProductService, {
         getOneBySlug: jest.fn().mockReturnValue(of(mockProduct)),
@@ -37,6 +50,11 @@ describe('ProductDetailComponent', () => {
     expect(spectator.component).toBeTruthy();
   });
 
+  it('should getOneBySlug be called', () => {
+    spectator.detectChanges();
+    expect(productService.getOneBySlug).toHaveBeenCalledWith(mockProduct.slug);
+  });
+
   it('should display the product cover', () => {
     // Act
     spectator.detectChanges();
@@ -45,5 +63,12 @@ describe('ProductDetailComponent', () => {
     const cover = spectator.query<HTMLImageElement>(byTestId('cover'));
     expect(cover).toBeTruthy();
     expect(cover?.src).toBe(mockProduct.images[0]);
+  });
+
+  it('should load related products', async () => {
+    spectator.detectChanges();
+    await spectator.deferBlock().renderComplete();
+    const related = spectator.query(RelatedComponent);
+    expect(related).toBeTruthy();
   });
 });
