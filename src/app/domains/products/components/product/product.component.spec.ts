7524e8ad4573150035f7804b416afa7d13deaddb
Patricio Pilco <pattopilco@gmail.com>
2025-04-13 06:36:44 +0000

    test espia a mmetodos

--- src/app/domains/products/components/product/product.component.spec.ts
+++ src/app/domains/products/components/product/product.component.spec.ts
@@ -1,24 +1,46 @@
-import { createRoutingFactory, SpectatorRouting } from '@ngneat/spectator/jest';
+import { byTestId, createRoutingFactory, SpectatorRouting } from '@ngneat/spectator/jest';
 import { ProductComponent } from './product.component';
 import { generateFakeProduct } from '@shared/models/product.mock';
+import { get } from 'http';
 
 describe('ProductComponent', () => {
   let spectator: SpectatorRouting<ProductComponent>;
   const createComponent = createRoutingFactory({
     component: ProductComponent,
-    detectChanges: false
   });
+
+  const mockProduct = generateFakeProduct();
 
   beforeEach(() => {
     spectator = createComponent({
-        props: {
-            product: generateFakeProduct(),
-        }
-
+      detectChanges: false
     });
+    spectator.setInput('product', mockProduct);
   });
 
   it('should create', () => {
+    spectator.detectChanges();
     expect(spectator.component).toBeTruthy();
+  });
+
+  it('should display product title', () => {
+    spectator.detectChanges();
+    //const element = spectator.query('[data-testid="product-title"]');
+    const element = spectator.query(byTestId('product-title'));
+    expect(element).toHaveText(mockProduct.title);
+  }
+  );
+
+  it('should emit a product when the button is clicked', () => {
+    //Arrange
+    spectator.detectChanges();
+
+    //Act
+    const emitSpy = jest.spyOn(spectator.component.addToCart, 'emit');
+    //spectator.component.addToCartHandler();
+    spectator.click(byTestId('add-to-cart-button'));
+    
+    //Assert
+    expect(emitSpy).toHaveBeenCalledWith(mockProduct);
   });
 });
